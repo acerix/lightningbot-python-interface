404de2bc79916a490d989cb31ba3ab1292c7e32c
acerix <dylan@psilly.com>
2018-11-20 22:19:25 -0500

    silence!

--- LightningBot.py
+++ LightningBot.py
@@ -504,7 +504,7 @@ class LightningBot:
 
       depth = self.longestWallPathDepth(position, try_direction, [], )
 
-      print('turn', turn_direction, 'depth', depth)
+      #print('turn', turn_direction, 'depth', depth)
 
       if depth > max_depth:
         max_depth = depth
